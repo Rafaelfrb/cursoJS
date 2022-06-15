60c19842fc73a6c8e64e40c8633cd5b179974c38
Rafaelfrb <rafael.frb@hotmail.com>
2022-06-15 14:46:06 -0300

    While and do while tests

---
+++ exrepeticoes.js
@@ -0,0 +1,13 @@
+var contador1 = 1;
+
+while (contador1 < 6) {
+    console.log(`Passo ${contador1}.`)
+    contador1++
+};
+
+var contador2 = 1;
+
+do {
+    console.log(`Teste do contador: ${contador2}.`)
+    contador2++
+} while (contador2 < 7);
